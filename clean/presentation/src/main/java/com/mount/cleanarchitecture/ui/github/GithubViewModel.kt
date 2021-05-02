4773a6eaaa0bc371ec382725c1a6c3594a5cb8f8
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2021-05-02 14:30:47 +0900

    [UPDATE] Github Fragment View

--- clean/presentation/src/main/java/com/mount/cleanarchitecture/ui/github/GithubViewModel.kt
+++ clean/presentation/src/main/java/com/mount/cleanarchitecture/ui/github/GithubViewModel.kt
@@ -1,6 +1,9 @@
 package com.mount.cleanarchitecture.ui.github
 
+import androidx.lifecycle.MutableLiveData
 import com.mount.cleanarchitecture.base.BaseViewModel
+import com.mount.cleanarchitecture.base.BindingItem
+import com.mount.cleanarchitecture.base.Event
 import com.mount.cleanarchitecture.ui.item.UserItemNavigator
 import com.mount.domain.model.User
 import com.mount.domain.usecase.AddBookmarkUserUseCase
@@ -10,6 +13,14 @@ class GithubViewModel(
     private val getAllSearchUserUseCase: GetAllSearchUserUseCase,
     private val addBookmarkUserUseCase: AddBookmarkUserUseCase
 ) : BaseViewModel(), UserItemNavigator {
+
+    val inputText = MutableLiveData<String>("")
+    val userName = MutableLiveData<String>("")
+
+    val userList = MutableLiveData<ArrayList<BindingItem>>()
+
+    val isLoading = MutableLiveData<Boolean>(false)
+    val onErrorEvent = MutableLiveData<Event<String>>()
 
     override fun onClickBookmark(user: User) {
         TODO("Not yet implemented")
